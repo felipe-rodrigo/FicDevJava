210655307ba7f30a1e1276d05109e9e8426de25f
felipe-rodrigo <105744545+felipe-rodrigo@users.noreply.github.com>
2023-06-27 10:22:17 -0400

    Add files via upload

---
+++ biblioteca02/src/main/java/com/example/demo/Biblioteca02Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biblioteca02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biblioteca02Application.class, args);
+	}
+
+}
